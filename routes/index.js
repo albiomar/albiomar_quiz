bb3e8126428d744aa0cde8ee56c7d25a0763413b
Alberto2 <alberto2@alberto2-VirtualBox.(none)>
2015-06-18 21:00:16 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var albiomar_quizController = require('../controllers/albiomar_quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/ventanas/question', albiomar_quizController.question);
+router.get('/ventanas/answer', albiomar_quizController.answer);
 
 module.exports = router;
