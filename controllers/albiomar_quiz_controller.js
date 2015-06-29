5b73ddebd27619287789b727ce012b208870f1ae
Alberto Biosca <bioscamarti@gmail.com>
2015-06-29 14:29:41 +0200

    Despliegue en Heroku

--- controllers/albiomar_quiz_controller.js
+++ controllers/albiomar_quiz_controller.js
@@ -1,15 +1,19 @@
 // Get /ventanas/question de index.js
+var models = require('../models/models.js');
 exports.question = function (req, res) {
-	res.render ('ventanas/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz){
+	res.render ('ventanas/question', {pregunta: quiz[0].pregunta});
+	})
 };
 // Get /ventanas/answer de index.js
 exports.answer = function (req, res) {
-	console.log(req.query.respuesta);
-	if (req.query.respuesta === 'Roma') {
-		res.render ('ventanas/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render ('ventanas/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render ('ventanas/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render ('ventanas/answer', {respuesta: 'Incorrecto'});
+		}
+	})
 };
 exports.author = function (req, res){
 	res.render ('ventanas/author')
